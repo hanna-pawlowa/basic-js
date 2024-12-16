781c3e90b2260385366ba8e8508f7f3b403c394d
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2024-12-16 19:43:12 +0200

    solve some tasks

--- src/what-season.js
+++ src/what-season.js
@@ -2,86 +2,57 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
+ * @returns {string} time of the year
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-//const isValidDateFormat = (date) =>{
-//  const dateRegexes = [
-//      /^\\d{4}-\\d{2}-\\d{2}$/,
-//      /^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-(\\d{4})$/,
-//      /^(0[1-9]|1[0-2])\/(0[1-9]|1[0-2])\/\d{4}$/
-//  ]
-//
-//  return dateRegexes.some((regex)=> regex.test(date)) ? "true" : "false"
-//}
-//
-//const isValidDate = (str) =>  {
-//  const date = new Date(str);
-//
-//  return (date instanceof Date) && !isNaN(date) && isValidDateFormat(str);
-//}
 
 function getSeason(date) {
-  //const timestamp = date.getDate()
-  //const isValidDate = isNaN(timestamp) || Number.isFinite(timestamp)
-  //
-  //const validDate= isValidDate(date)
-  //
-  //if (!validDate) {
-  //  throw new Error('Invalid date!')
-  //}
+  if (!date) {
+    throw new Error('Invalid date!');
+  }
 
-  //const seasons = ['winter', 'spring', 'summer', 'fall']
-  //let season = ''
-  //
-  //const oneBasedMonth = date.getMonth() + 1
-  //
-  //switch(oneBasedMonth) {
-  //  case 1:
-  //    season = seasons[0]
-  //    break
-  //  case 2:
-  //    season = seasons[0]
-  //    break
-  //  case 3:
-  //    season = seasons[1]
-  //    break
-  //  case 4:
-  //    season = seasons[1]
-  //    break
-  //  case 5:
-  //    season = seasons[1]
-  //    break
-  //  case 6:
-  //    season = seasons[2]
-  //    break
-  //  case 7:
-  //    season = seasons[2]
-  //    break
-  //  case 8:
-  //    season = seasons[2]
-  //    break
-  //  case 9:
-  //    season = seasons[3]
-  //    break
-  //  case 10:
-  //    season = seasons[3]
-  //    break
-  //  case 11:
-  //    season = seasons[3]
-  //    break
-  //  case 12:
-  //    season = seasons[0]
-  //    break
-  //}
-  //
-  //return season
+  if (typeof date === 'string') {
+    date = new Date(date);
+  }
+
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    throw new Error('Invalid date!');
+  }
+
+  const seasons = ['winter', 'spring', 'summer', 'fall'];
+
+  const month = date.getMonth();
+
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return seasons[0];
+
+    case 2:
+    case 3:
+    case 4:
+      return seasons[1];
+
+    case 5:
+    case 6:
+    case 7:
+     return seasons[2];
+
+    case 8:
+    case 9:
+    case 10:
+      return seasons[3];
+
+    default:
+      throw new Error('Invalid date!');
+  }
 }
 
 module.exports = {
