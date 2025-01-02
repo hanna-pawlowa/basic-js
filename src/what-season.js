2bb2ff895926e23abfd73d0f741402a379bcd314
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-01-02 10:27:54 +0200

    update getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -14,44 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 function getSeason(date) {
   if (!date) {
-    throw new Error('Invalid date!');
+    return 'Unable to determine the time of year!';
   }
 
-  if (typeof date === 'string') {
-    date = new Date(date);
-  }
-
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
-    throw new Error('Invalid date!');
-  }
-
-  const seasons = ['winter', 'spring', 'summer', 'fall'];
-
-  const month = date.getMonth();
-
-  switch (month) {
-    case 11:
-    case 0:
-    case 1:
-      return seasons[0];
-
-    case 2:
-    case 3:
-    case 4:
-      return seasons[1];
-
-    case 5:
-    case 6:
-    case 7:
-     return seasons[2];
-
-    case 8:
-    case 9:
-    case 10:
-      return seasons[3];
-
-    default:
+  try {
+    if (
+        !(date instanceof Date) ||
+        isNaN(date.getTime())
+    ) {
       throw new Error('Invalid date!');
+    }
+
+    const month = date.getMonth();
+    const seasons = ['winter', 'spring', 'summer', 'fall'];
+
+    switch (month){
+      case 11:
+      case 0:
+      case 1:
+        return seasons[0];
+
+      case 2:
+      case 3:
+      case 4:
+        return seasons[1];
+
+      case 5:
+      case 6:
+      case 7:
+        return seasons[2];
+
+      case 8:
+      case 9:
+      case 10:
+        return seasons[3];
+    }
+  } catch {
+    throw new Error('Invalid date!');
   }
 }
 
